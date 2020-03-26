5aab554b112147faabb2e8bdcb353608d0a1db0d
leavest007 <leavest007@gmail.com>
2020-03-26 19:23:57 +0700

    master

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,51 @@
+<footer class="section-footer mt-5 mb-4 border-top">
+        <div class="container pt-5 pb-5">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="col-12 col-lg-3">
+                            <h5>FEATURES</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Reviews</a></li>
+                                <li><a href="#">Community</a></li>
+                                <li><a href="#">Program</a></li>
+                                <li><a href="#">Afilliate</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-lg-3">
+                            <h5>ACCOUNTS</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Refund</a></li>
+                                <li><a href="#">Security</a></li>
+                                <li><a href="#">Rewards</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-lg-3">
+                            <h5>COMPANY</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Careers</a></li>
+                                <li><a href="#">Help Center</a></li>
+                                <li><a href="#">Media</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-12 col-lg-3">
+                            <h5>GET CONNECTED</h5>
+                            <ul class="list-unstyled">
+                                <li><a href="#">Bandung, West Java</a></li>
+                                <li><a href="#">Indonesia</a></li>
+                                <li><a href="#">0821 - 2048 - 7104</a></li>
+                                <li><a href="#">info@yashmatours.com</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row border-top justify-content-center align-items-center pt-4">
+                <div class="col-auto text-gray-500 font-weight-light">
+                    2019 Copyright Yashmatours • All rights reserved • Made in Bandung
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,67 @@
+  <!-- Navbar -->
+    <div class="container">
+        <nav class="row navbar navbar-expand-lg navbar-light bg-white">
+            <a href="#" class="navbar-brand">
+                <img src="frontend/images/Logo.png" alt="Logo NMADS" class="">
+            </a>
+            <button 
+                class="navbar-toggler navbar-toggler-right" 
+                type="button" 
+                data-toggle="collapse"
+                data-target="#navheader">
+            </button>
+
+            <div class="collapse navbar-collapse" id="navheader">
+                <ul class="navbar-nav ml-auto mr-3">
+                    <li class="nav-item mx-md-2"><a href="#" class="nav-link active">Home</a></li>
+                    <li class="nav-item mx-md-2"><a href="#" class="nav-link">Paket Travel</a></li>
+                    <li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" id="navbardrop"
+                            data-toggle="dropdown">Services</a>
+                        <div class="dropdown-menu">
+                            <a href="#" class="dropdown-item">Umroh</a>
+                            <a href="#" class="dropdown-item">Haji</a>
+                            <a href="#" class="dropdown-item">Travel</a>
+                        </div>
+                    </li>
+                    <li class="nav-item mx-md-2"><a href="#" class="nav-link">Testimonials</a></li>
+                </ul>
+
+                @guest
+                <!--- Mobile Button --->
+                <form class="form-inline d-sm-block d-md-none">
+                    @csrf
+                <button class="btn btn-login my-2 my-sm-0" type="button" onclick="event.preventDefault(); location.href='{{ url('login')}}';">
+                        Masuk
+                    </button>
+                </form>
+
+                <!--- Dekstop Button --->
+                <form class="form-inline my-2 my-lg-0 d-none d-md-block">
+                    @csrf
+                <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="button" onclick="event.preventDefault(); location.href='{{ url('login')}}';">
+                        Masuk
+                    </button>
+                </form>
+                @endguest
+
+
+                @auth
+                <!--- Mobile Button --->
+                <form class="form-inline d-sm-block d-md-none" action="{{ url('logout')}}" method="POST">
+                    @csrf
+                    <button class="btn btn-login my-2 my-sm-0" type="submit">
+                        Keluar
+                    </button>
+                </form>
+
+                <!--- Dekstop Button --->
+                <form class="form-inline my-2 my-lg-0 d-none d-md-block" action="{{ url('logout')}}" method="POST">
+                    @csrf
+                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="submit">
+                        Keluar
+                    </button>
+                </form>
+                @endauth
+            </div>
+        </nav>
+    </div>

7bf4023964cc3609f53c3413306e5aac89722cd1
leavest007 <leavest007@gmail.com>
2020-03-27 20:13:58 +0700

    Gallery Section - Delete

--- app/Http/Controllers/Admin/GalleryController.php
+++ app/Http/Controllers/Admin/GalleryController.php
@@ -76,9 +76,11 @@ class GalleryController extends Controller
     public function edit($id)
     {
         $item = Gallery::findOrFail($id);
+        $travel_packages = TravelPackage::all();
 
         return view('pages.admin.gallery.edit', [
-            'item' => $item
+            'item' => $item,
+            'travel_packages' => $travel_packages
         ]);
     }
 
@@ -92,7 +94,10 @@ class GalleryController extends Controller
     public function update(GalleryRequest $request, $id)
     {
         $data = $request->all();
-        $data['slug'] = Str::slug($request->title);
+        $data['image'] = $request->file('image')->store(
+            'assets/gallery',
+            'public'
+        );
 
         $item = Gallery::findOrFail($id);
         $item->update($data);
